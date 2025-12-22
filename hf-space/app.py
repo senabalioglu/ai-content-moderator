0b6d537f9c44720c48a7e47fba0a1eb65d47f9ee
Sena Balioğlu <127862385+senabalioglu@users.noreply.github.com>
2025-12-23 01:57:18 +0300

    hf-space
    hf-space removed

--- hf-space/app.py
+++
@@ -1,9 +0,0 @@
-import gradio as gr
-
-print("HF Space rebuild test")
-
-def greet(name):
-    return "Hello " + name + "!!"
-
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-demo.launch()
